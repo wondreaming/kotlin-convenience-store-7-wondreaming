1c0f9c7b4c6f60a55045b39f977e0521aa04ec1e
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-07 23:04:50 +0900

    feat: 사용자 입력과 출력을 관리하는 UserInteractionController 생성
    - InputView와 OutputView 사이에서 사용자 상호작용을 관리하는 컨트롤러 추가
    - 구매 입력, 프로모션 확인, 정가 결제 확인, 멤버십 할인 여부, 추가 구매 여부를 처리하는 메서드 구현

--- src/main/kotlin/store/controller/UserInteractionController.kt
+++ src/main/kotlin/store/controller/UserInteractionController.kt
@@ -1,4 +1,39 @@
 package store.controller
 
-class UserInteractionController {
+import store.view.InputView
+import store.view.OutputView
+
+class UserInteractionController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+    fun handlePurchaseInput(productsInfo: List<String>): String {
+        outputView.showProductInfo(productsInfo)
+        val purchaseInput = inputView.getPurchaseInput()
+        return purchaseInput
+    }
+
+    fun handlePromotionConfirmation(name: String, quantity: Int): String {
+        outputView.showPromotionAdditionalOffer(name, quantity)
+        val userInput = inputView.getPromotionConfirmation()
+        return userInput
+    }
+
+    fun handleFullPriceConfirmation(name: String, quantity: Int): String {
+        outputView.showPromotionStockShortage(name, quantity)
+        val userInput = inputView.getFullPriceConfirmation()
+        return userInput
+    }
+
+    fun handleMembershipDiscount(): String {
+        outputView.showMembershipDiscount()
+        val userInput = inputView.getMembershipDiscount()
+        return userInput
+    }
+
+    fun handleAdditionalPurchase(): String {
+        outputView.showAdditionalPurchase()
+        val userInput = inputView.getAdditionalPurchase()
+        return userInput
+    }
 }
