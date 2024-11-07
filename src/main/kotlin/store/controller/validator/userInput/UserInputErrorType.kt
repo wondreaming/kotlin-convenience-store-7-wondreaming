4ae4b15fdb248ea58e2ff07f618cb0a6b3f8ffc1
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-07 18:13:02 +0900

    style: UserInputErrorType의 오류 메시지 문법 수정

--- src/main/kotlin/store/controller/validator/userInput/UserInputErrorType.kt
+++ src/main/kotlin/store/controller/validator/userInput/UserInputErrorType.kt
@@ -5,8 +5,8 @@ enum class UserInputErrorType(
 ) {
     EMPTY_INPUT("빈 문자를 입력했습니다. 다시 입력해 주세요."),
     LOWERCASE("소문자를 입력했습니다. 다시 입력해 주세요"),
-    KOREAN("한국어를 입력했습니다. 다시 입력해주세요"),
-    INVALID_CHAR("Y 또는 N만 입력이 가능합니다. 다시 입력해주세요");
+    KOREAN("한국어를 입력했습니다. 다시 입력해 주세요"),
+    INVALID_CHAR("Y 또는 N만 입력이 가능합니다. 다시 입력해 주세요");
 
     val errorMessage: String
         get() = ERROR_MESSAGE + _errorMessage
