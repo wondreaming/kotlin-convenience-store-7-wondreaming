cc5f3beebeca3ac441d360edd9a676d13ab57811
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-07 17:16:09 +0900

    feat: 프로모션 추가 혜택 안내 메시지 출력 기능 추가
    - 고객이 프로모션 혜택 수량보다 적게 구매할 경우 추가 혜택 안내 메시지 출력

--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -8,11 +8,17 @@ class OutputView {
         println(NEW_LINE + PURCHASE_PROMPT_MESSAGE)
     }
 
+    fun showPromotionAdditionalOffer(name: String, quantity: Int) {
+        val formattedMessage = String.format(PROMOTION_ADDITIONAL_OFFER_MESSAGE, name, quantity)
+        println(formattedMessage)
+    }
+
     companion object {
         private const val WELCOME_MESSAGE: String = "안녕하세요. W편의점입니다."
         private const val PRODUCT_AVAILABILITY_MESSAGE: String = "현재 보유하고 있는 상품입니다."
         private const val NEW_LINE = "\n"
         private const val PURCHASE_PROMPT_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"
+        private const val PROMOTION_ADDITIONAL_OFFER_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
 
     }
 }
