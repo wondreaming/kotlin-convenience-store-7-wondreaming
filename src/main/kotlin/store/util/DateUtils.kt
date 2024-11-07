b194a057125d1882184cee7b57b4f518daeaf297
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-07 14:10:15 +0900

    refactor: DateUtils에서 날짜 패턴 상수를 추가하여 코드 가독성 향상
    - DATE_PATTERN 상수를 추가하여 날짜 포맷 패턴을 명확히 정의
    - DateTimeFormatter에서 상수를 사용해 코드 가독성과 유지보수성 개선

--- src/main/kotlin/store/util/DateUtils.kt
+++ src/main/kotlin/store/util/DateUtils.kt
@@ -4,8 +4,10 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 object DateUtils {
+    private const val DATE_PATTERN = "yyyy-MM-dd"
+
     fun stringToLocalDate(dateString: String): LocalDate {
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val formatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
         return LocalDate.parse(dateString, formatter)
     }
 }
