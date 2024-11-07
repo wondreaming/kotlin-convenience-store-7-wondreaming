f5a3a22a985c966f42fc64bbd96059b79151d2bd
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-07 14:52:49 +0900

    feat: 구매할 상품과 수량을 입력받는 기능 추가
    - 사용자가 구매하고자 하는 상품명과 수량을 입력받는 기능 구현

--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -1,4 +1,19 @@
 package store.view
 
+import camp.nextstep.edu.missionutils.Console
+
 object InputView {
+    fun getPurchaseInput(): String {
+        val purchaseInput = getInput()
+        return purchaseInput
+    }
+
+    private fun getInput(): String {
+        val input = Console.readLine()
+        return input
+    }
+
+    fun closeInput() {
+        Console.close()
+    }
 }
