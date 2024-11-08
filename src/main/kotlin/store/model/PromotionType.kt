8e04bbf73140065a1366bc1c9f4a1092d22af9ab
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-08 14:47:01 +0900

    refactor: PromotionType의 날짜 타입을 LocalDate에서 LocalDateTime으로 변경
    - PromotionType의 startDate와 endDate를 LocalDateTime으로 변경하여 시간 정보 포함

--- src/main/kotlin/store/model/PromotionType.kt
+++ src/main/kotlin/store/model/PromotionType.kt
@@ -1,12 +1,13 @@
 package store.model
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 class PromotionType(
     val name: String,
     val buyQuantity: Int,
     val freeQuantity: Int,
-    val startDate: LocalDate,
-    val endDate: LocalDate,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
 ) {
 }
--- src/main/kotlin/store/util/DateUtils.kt
+++ src/main/kotlin/store/util/DateUtils.kt
@@ -1,13 +1,19 @@
 package store.util
 
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 object DateUtils {
-    private const val DATE_PATTERN = "yyyy-MM-dd"
+    private const val DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss"
 
-    fun stringToLocalDate(dateString: String): LocalDate {
-        val formatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
-        return LocalDate.parse(dateString, formatter)
+    fun stringToLocalDateTime(dateTimeString: String): LocalDateTime {
+        val formattedDateTimeString = if (dateTimeString.length == 10) {
+            "$dateTimeString 00:00:00"
+        } else {
+            dateTimeString
+        }
+
+        val formatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)
+        return LocalDateTime.parse(formattedDateTimeString, formatter)
     }
 }
