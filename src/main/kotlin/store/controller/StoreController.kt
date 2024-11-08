9d4823585e944957772b33024b2d816c3490c299
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-09 01:09:53 +0900

    feat: StoreController 및 main 함수에서 상점 프로그램 초기화 및 실행 구조 설정

--- src/main/kotlin/store/controller/StoreController.kt
+++ src/main/kotlin/store/controller/StoreController.kt
@@ -1,4 +1,40 @@
 package store.controller
 
-class StoreController {
+import store.controller.adapter.ProductAdapter
+import store.controller.adapter.PurchaseInfoAdapter
+import store.controller.featurecontroller.ProductController
+import store.controller.featurecontroller.PromotionTypeController
+import store.controller.featurecontroller.PurchaseController
+import store.controller.featurecontroller.UserInteractionController
+import store.controller.validator.PurchaseInputValidator
+import store.controller.validator.userInput.UserInputValidator
+import store.view.InputView
+import store.view.OutputView
+
+class StoreController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(),
+    private val productAdapter: ProductAdapter = ProductAdapter(),
+    private val promotionTypeController: PromotionTypeController = PromotionTypeController(),
+    private val productController: ProductController = ProductController(promotionTypeController, productAdapter),
+    private val userInteractionController: UserInteractionController = UserInteractionController(inputView, outputView),
+    private val userInputValidator: UserInputValidator = UserInputValidator(),
+    private val purchaseController: PurchaseController = PurchaseController(userInteractionController, userInputValidator),
+) {
+    fun run() {
+        val products = productController.loadProducts()
+        val productsInfo = productController.adaptProducts(products)
+
+        val purchaseInput = userInteractionController.handlePurchaseInput(productsInfo)
+        val purchaseInputValidator = PurchaseInputValidator(purchaseInput, products)
+
+        purchaseInputValidator.validate()
+
+        val purchaseInfoAdapter = PurchaseInfoAdapter(purchaseInput)
+        val purchaseInfos = purchaseInfoAdapter.adaptPurchaseInfo()
+
+        // 프로모션 확인과 재고 확인 처리
+        val updatedPurchaseInfos = purchaseController.checkPromotionPurchase(purchaseInfos, products)
+        val finalPurchaseInfos = purchaseController.checkPromotionStockSufficient(updatedPurchaseInfos, products)
+    }
 }
