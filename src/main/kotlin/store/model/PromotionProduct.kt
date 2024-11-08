2f172a9ccdab56fbfa35c531f2a126edd9998bba
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-08 14:57:01 +0900

    feat: PromotionProduct에 프로모션 활성 여부 확인 기능 추가
    - 오늘 날짜가 프로모션 기간(startDate와 endDate) 내에 포함되는 경우에만 프로모션이 적용되도록 isPromotionActive 함수 추가
    - DateTimes.now()를 사용하여 현재 날짜와 프로모션 기간을 비교

--- src/main/kotlin/store/model/PromotionProduct.kt
+++ src/main/kotlin/store/model/PromotionProduct.kt
@@ -1,5 +1,7 @@
 package store.model
 
+import camp.nextstep.edu.missionutils.DateTimes
+
 data class PromotionProduct(
     override val price: Int,
     private var _quantity: Int,
@@ -9,4 +11,12 @@ data class PromotionProduct(
         get() = _quantity
     val promotionType: PromotionType
         get() = _promotionType
+
+    // 제품이 프로모션 날짜에 속하는 지 아닌지
+    fun isPromotionActive(): Boolean {
+        val today = DateTimes.now()
+        val isPromotionActiveToday = today.isAfter(_promotionType.startDate) && today.isBefore(promotionType.endDate)
+        return isPromotionActiveToday
+    }
+
 }
