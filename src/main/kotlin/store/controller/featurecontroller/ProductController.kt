a7d8acee4c7f6cd378741a3c2b31d92163041ccf
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-11 04:41:36 +0900

    refactor: ProductController 가독성있게 변경

--- src/main/kotlin/store/controller/featurecontroller/ProductController.kt
+++ src/main/kotlin/store/controller/featurecontroller/ProductController.kt
@@ -13,72 +13,41 @@ class ProductController(
         val products = mutableMapOf<String, Product>()
         File(PRODUCTS_FILE_PATH).readLines()
             .filterNot { it.startsWith(HEADER_NAME) }
-            .forEach { line ->
-                processLine(line, products)
-            }
+            .forEach { processLine(it, products) }
         return products
     }
 
     private fun processLine(line: String, products: MutableMap<String, Product>) {
         val (name, price, quantity, promotionTypeName) = line.split(DELIMITER)
-        if (!products.containsKey(name)) {
-            products[name] = createProduct(name, price, quantity, promotionTypeName)
-            return
+        val product = products[name] ?: createProduct(name, price, quantity, promotionTypeName)
+        products[name] = product.apply {
+            if (products.containsKey(name)) updateExistingProduct(this, price, quantity, promotionTypeName)
         }
-        updateExistingProduct(products[name]!!, price, quantity, promotionTypeName)
     }
 
     private fun createProduct(name: String, price: String, quantity: String, promotionTypeName: String): Product {
-        if (promotionTypeName != NO_PROMOTION_LABEL) {
-            val promotionTypes = findPromotionType(promotionTypeName)
-            if (promotionTypes.isPromotionActive()) {
-                val promotionProduct = parsePromotionProduct(price, quantity, promotionTypeName)
-                val nonPromotionProduct = parseNonPromotionProduct(price, ZERO)
-                return Product(name, promotionProduct, nonPromotionProduct)
+        return if (isPromotionAvailable(promotionTypeName))
+            Product(name, parsePromotionProduct(price, quantity, promotionTypeName), parseNonPromotionProduct(price, ZERO))
+        else Product(name, null, parseNonPromotionProduct(price, quantity))
+    }
 
-            }
+    private fun updateExistingProduct(product: Product, price: String, quantity: String, promotionTypeName: String) {
+        when {
+            isPromotionProductUpdateNeeded(product, promotionTypeName) -> updatePromotionProduct(product, price, quantity, promotionTypeName)
+            isNonPromotionProductCreationNeeded(product, promotionTypeName) -> updateNonPromotionProduct(product, price, quantity)
+            else -> increaseNonPromotionQuantity(product, quantity)
         }
-        val nonPromotionProduct = parseNonPromotionProduct(price, quantity)
-        return Product(name, null, nonPromotionProduct)
     }
 
-    private fun updateExistingProduct(
-        product: Product,
-        price: String,
-        quantity: String,
-        promotionTypeName: String
-    ) {
-        if (isPromotionProductUpdateNeeded(product, promotionTypeName)) {
-            updatePromotionProduct(product, price, quantity, promotionTypeName)
-            return
-        }
+    private fun isPromotionAvailable(promotionTypeName: String) = promotionTypeName != NO_PROMOTION_LABEL && findPromotionType(promotionTypeName).isPromotionActive()
 
-        if (isNonPromotionProductCreationNeeded(product, promotionTypeName)) {
-            updateNonPromotionProduct(product, price, quantity)
-            return
-        }
+    private fun isPromotionProductUpdateNeeded(product: Product, promotionTypeName: String) =
+        isPromotionAvailable(promotionTypeName) && product.promotionProduct == null
 
-        increaseNonPromotionQuantity(product, quantity)
-    }
+    private fun isNonPromotionProductCreationNeeded(product: Product, promotionTypeName: String) =
+        (promotionTypeName == NO_PROMOTION_LABEL || !isPromotionAvailable(promotionTypeName)) && product.nonPromotionProduct == null
 
-    private fun isPromotionProductUpdateNeeded(product: Product, promotionTypeName: String): Boolean {
-        return promotionTypeName != NO_PROMOTION_LABEL &&
-                findPromotionType(promotionTypeName).isPromotionActive() &&
-                product.promotionProduct == null
-    }
-
-    private fun isNonPromotionProductCreationNeeded(product: Product, promotionTypeName: String): Boolean {
-        return promotionTypeName == NO_PROMOTION_LABEL ||
-                !findPromotionType(promotionTypeName).isPromotionActive() &&
-                product.nonPromotionProduct == null
-    }
-
-    private fun updatePromotionProduct(
-        product: Product,
-        price: String,
-        quantity: String,
-        promotionTypeName: String
-    ) {
+    private fun updatePromotionProduct(product: Product, price: String, quantity: String, promotionTypeName: String) {
         product.updatePromotionProduct(parsePromotionProduct(price, quantity, promotionTypeName))
     }
 
@@ -90,18 +59,12 @@ class ProductController(
         product.updateNonPromotionQuantity(quantity.toInt())
     }
 
-    private fun parsePromotionProduct(
-        price: String,
-        quantity: String,
-        promotionTypeName: String,
-    ): PromotionProduct {
-        val promotionType = findPromotionType(promotionTypeName)
-        return PromotionProduct(price.toInt(), quantity.toInt(), promotionType)
+    private fun parsePromotionProduct(price: String, quantity: String, promotionTypeName: String): PromotionProduct {
+        return PromotionProduct(price.toInt(), quantity.toInt(), findPromotionType(promotionTypeName))
     }
 
     private fun findPromotionType(promotionTypeName: String): PromotionType {
-        val promotionTypes = promotionTypeController.loadPromotionType()
-        return promotionTypes.find { it.name == promotionTypeName }!!
+        return promotionTypeController.loadPromotionType().find { it.name == promotionTypeName }!!
     }
 
     private fun parseNonPromotionProduct(price: String, quantity: String): NonPromotionProduct {
