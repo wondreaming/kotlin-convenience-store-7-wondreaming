df58ba32d443be8381a885d50be25fae6930cf14
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-10 21:02:52 +0900

    refactor: UserInputValidator 상수화

--- src/main/kotlin/store/controller/validator/userInput/UserInputValidator.kt
+++ src/main/kotlin/store/controller/validator/userInput/UserInputValidator.kt
@@ -5,6 +5,11 @@ import store.controller.validator.userInput.UserInputErrorType.*
 class UserInputValidator {
     fun validateUserInput(userInput: String) {
         require(userInput.isNotEmpty()) { EMPTY_INPUT.errorMessage }
-        require(userInput == "Y" || userInput == "N") { INVALID_CHAR.errorMessage }
+        require(userInput == YES || userInput == NO) { INVALID_CHAR.errorMessage }
+    }
+
+    companion object {
+        private const val YES = "Y"
+        private const val NO = "N"
     }
 }
